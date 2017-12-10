f1f376781cceb7bb2ca3a001cf317081cda0d95d
Neil Dave <dave.ne@husky.neu.edu>
2017-12-10 00:19:07 -0500

    Added structure to, simultaneously record april tag coordinates and navigate around map, beginnings of tf transform

--- mobile_robotics_ws/src/waypointnav/scripts/store_april_tags_2_classes.py
+++ mobile_robotics_ws/src/waypointnav/scripts/store_april_tags_2_classes.py
@@ -18,7 +18,6 @@ from tf import TransformListener
 
 
 detected = [False, False, False, False, False, False]
-initialrun = True;
 basex        = "0"
 basey        = "0"
 basez        = "0"
@@ -30,61 +29,9 @@ rotationx    = ["0","0","0","0","0","0"]
 rotationy    = ["0","0","0","0","0","0"]
 rotationz    = ["0","0","0","0","0","0"]
 rotationw    = ["0","0","0","0","0","0"]
-     
-   
-class GoToPose():
-    
-    def __init__(self):
-        #self.tf = TransformListener()
-        self.goal_sent = False
 
-	# What to do if shut down (e.g. Ctrl-C or failure)
-	rospy.on_shutdown(self.shutdown)
-	
-   
-  
-	# Tell the action client that we want to spin a thread by default
-	self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-	rospy.loginfo("Wait for the action server to come up")
+class AprilTagListener():
 
-	# Allow up to 5 seconds for the action server to come up
-	self.move_base.wait_for_server(rospy.Duration(5))
-
-
-    def goto(self, pos, quat):
-
-        # Send a goal
-        self.goal_sent = True
-	goal = MoveBaseGoal()
-	goal.target_pose.header.frame_id = 'map'
-	goal.target_pose.header.stamp = rospy.Time.now()
-        goal.target_pose.pose = Pose(Point(pos['x'], pos['y'], 0.000),
-                                     Quaternion(quat['r1'], quat['r2'], quat['r3'], quat['r4']))
-
-	# Start moving
-        self.move_base.send_goal(goal)
-
-	# Allow TurtleBot up to 60 seconds to complete task
-	success = self.move_base.wait_for_result(rospy.Duration(60)) 
-
-        state = self.move_base.get_state()
-        result = False
-
-        if success and state == GoalStatus.SUCCEEDED:
-            # We made it!
-            result = True
-        else:
-            self.move_base.cancel_goal()
-
-        self.goal_sent = False
-        return result
-
-    def shutdown(self):
-        if self.goal_sent:
-            self.move_base.cancel_goal()
-        rospy.loginfo("Stop")
-        rospy.sleep(1)
-        
     def callback(self,data):
         rospy.loginfo(data.transforms[0])
 ##        if self.tf.frameExists("/base_link") and self.tf.frameExists("/map"):
@@ -140,29 +87,79 @@ class GoToPose():
           rospy.loginfo(translationx);
 
     def listener(self):
-        global initialrun
-        position = {'x': .5, 'y' : 1}
-        quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
         #rospy.init_node('listener', anonymous = True)
-        rospy.sleep(30); #Game plan for now is to map tags in teleop, store coordinates, when all tags hit, sleep, close out of teleop, then begin navigtion routine to all tags.
         rospy.Subscriber('tf',TFMessage,self.callback)
-        if initialrun == True:
-            success = self.goto(position, quaternion)
-            initialrun = False
-        rospy.spin()
+       # rospy.spin()
+        
+   
+ 
+
+class GoToPose():
+    def __init__(self):
+        #self.tf = TransformListener()
+        self.goal_sent = False
+
+	# What to do if shut down (e.g. Ctrl-C or failure)
+	rospy.on_shutdown(self.shutdown)
+	
+   
+  
+	# Tell the action client that we want to spin a thread by default
+	self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+	rospy.loginfo("Wait for the action server to come up")
+
+	# Allow up to 5 seconds for the action server to come up
+	self.move_base.wait_for_server(rospy.Duration(5))
+
+
+    def goto(self, pos, quat):
+
+        # Send a goal
+        self.goal_sent = True
+	goal = MoveBaseGoal()
+	goal.target_pose.header.frame_id = 'map'
+	goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose = Pose(Point(pos['x'], pos['y'], 0.000),
+                                     Quaternion(quat['r1'], quat['r2'], quat['r3'], quat['r4']))
+
+	# Start moving
+        self.move_base.send_goal(goal)
+
+	# Allow TurtleBot up to 60 seconds to complete task
+	success = self.move_base.wait_for_result(rospy.Duration(60)) 
+
+        state = self.move_base.get_state()
+        result = False
+
+        if success and state == GoalStatus.SUCCEEDED:
+            # We made it!
+            result = True
+        else:
+            self.move_base.cancel_goal()
+
+        self.goal_sent = False
+        return result
+
+    def shutdown(self):
+        if self.goal_sent:
+            self.move_base.cancel_goal()
+        rospy.loginfo("Stop")
+        rospy.sleep(1)
+
   
 
 if __name__ == '__main__':
     try:
         rospy.init_node('nav_test', anonymous=False)
         navigator = GoToPose()
+        detector = AprilTagListener();
         # Customize the following values so they are appropriate for your location
         position = {'x': .5, 'y' : 1}
         quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
 	rospy.loginfo(Point());
         rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
-        navigator.listener();
-        #success = navigator.goto(position, quaternion)
+        detector.listener();
+      #  success = navigator.goto(position, quaternion)
 
       #  if success:
        #     rospy.loginfo("Hooray, reached the desired pose")
